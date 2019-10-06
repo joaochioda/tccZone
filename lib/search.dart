c46be79145e2a581d704812a40513442d9608c15
joaochioda <joao.chioda@dextra-sw.com>
2019-10-05 22:47:42 -0300

    listing essencias and tapped

--- lib/search.dart
+++ lib/search.dart
@@ -28,15 +28,19 @@ class MySearchPage extends StatefulWidget {
 
 class _MySearchPageState extends State<MySearchPage> {
   List<Essencia> essenciaG = [];
-
-   _getUsers() async {
+  List<Marca> marcaG = [];
+  _getUsers() async {
     if (essenciaG.isEmpty) {
       var data =
           await http.get("https://pure-scrubland-45679.herokuapp.com/essencia");
 
+      var data1 =
+          await http.get("https://pure-scrubland-45679.herokuapp.com/marca");
       var jsonData = json.decode(data.body);
+      var jsonData1 = json.decode(data1.body);
 
       List<Essencia> users = [];
+      List<Marca> marca = [];
 
       for (var u in jsonData) {
         Marca marc = Marca(u["marca"][0]["id"], u["marca"][0]["marca"]);
@@ -46,27 +50,62 @@ class _MySearchPageState extends State<MySearchPage> {
 
         users.add(user);
       }
-      if(this.mounted){
-      setState(() {
-        essenciaG = users;
-      });
+
+      for (var u in jsonData1) {
+        Marca marc = Marca(u["id"], u["marca"]);
+        marca.add(marc);
+      }
+      if (this.mounted) {
+        setState(() {
+          essenciaG = users;
+          marcaG = marca;
+        });
       }
       return true;
     }
     return true;
   }
 
-  static final List<String> _listViewData = [
-    "sabado",
-    "cachorro",
-    "salame",
-  ];
+getEssencia(int j,int l) {
+  List<Essencia> ess = [];
 
+    for(int i=0;i<essenciaG.length;i++) {
+        if(essenciaG[i].marca.id == marcaG[j].id) {
+          ess.add(essenciaG[i]);
+  }
+    }
+    
+      return ess[l].sabor;
+
+}
+
+getWidth(int k) {
+  int contador=0;
+  for (int i=0;i<essenciaG.length;i++)
+  {
+    if(essenciaG[i].marca.id == marcaG[k].id) {
+      contador++;
+    }
+  }
+  return contador;
+}
+
+getTapped(int j,int l) {
+  List<Essencia> ess = [];
+
+    for(int i=0;i<essenciaG.length;i++) {
+        if(essenciaG[i].marca.id == marcaG[j].id) {
+          ess.add(essenciaG[i]);
+  }
+    }
+    
+      print("Voce clicou ${ess[l].sabor}");
+
+}
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Lista de essências")),
+        appBar: new AppBar(title: new Text("Lista de essências")),
         body: Container(
             child: FutureBuilder(
                 future: _getUsers(),
@@ -75,23 +114,23 @@ class _MySearchPageState extends State<MySearchPage> {
                     return Container(child: Center(child: Text("Loading...")));
                   } else {
                     return ListView.builder(
-                        itemCount: _listViewData == null ? 0 : 3,
-                        itemBuilder: (BuildContext context, int index) {
+                        itemCount: marcaG == null ? 0 : marcaG.length,
+                        itemBuilder: (BuildContext context, int marcaIndex) {
                           return Container(
                               child: ExpansionTile(
                             title: ListTile(
-                              title: Text(_listViewData[index]),
+                              title: Text(marcaG[marcaIndex].marca),
                             ),
                             children: <Widget>[
                               ListView.builder(
                                 itemBuilder: (context, index) {
                                   return Container(
                                       child: ListTile(
-                                    title: Text("oi"),
-                                    onTap: () => print("oi"),
+                                    title: Text(getEssencia(marcaIndex,index)),
+                                    onTap: () => getTapped(marcaIndex, index),
                                   ));
                                 },
-                                itemCount: 9,
+                                itemCount: getWidth(marcaIndex),
                                 shrinkWrap: true,
                                 physics: const ClampingScrollPhysics(),
                               )
